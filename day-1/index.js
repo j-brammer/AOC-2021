488bbba4ef8502ef987297f77baaaf59d1ca0469
Jason Brammer <jason.brammer@tensure.io>
2021-12-03 11:05:08 -0500

    Day 1 part 2 solution completed. Code refactored

--- day-1/index.js
+++ day-1/index.js
@@ -1,13 +1,27 @@
 const Files = require("../utils/files");
 const data = Files.readNumbersFromFile(__dirname + '/input.txt')
 
-upCount = 0;
-for (let i = 0; i <= data.length; i++){
-    if (i == 0){
-        console.log("Initial value; no calculation to be made");
-    } else if (data[i] > data[i - 1]) {
-        upCount++;
+function calculateSolution(data) {
+    solution = 0;
+    for (let i = 0; i <= data.length; i++){
+        if (i == 0){
+            console.log("Initial value; no calculation to be made");
+        } else if (data[i] > data[i - 1]) {
+            solution++;
+        }
     }
+    return solution;
 }
 
-console.log("Total count of increases in depth: ", upCount);
+var part1Solution = calculateSolution(data);
+
+var slidingDataSet = [];
+for (let i = 2; i < data.length; i++){
+    slidingDataSet.push(data[i] + data[i - 1] + data[i - 2]);
+}
+
+var part2Solution = calculateSolution(slidingDataSet);
+
+console.log("Part 1 Solution: ", part1Solution);
+console.log("Part 2 Solution: ", part2Solution);
+
